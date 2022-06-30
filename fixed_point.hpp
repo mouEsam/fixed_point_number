3883de297f7e2523c610e8dd12668361d78eeef3
Moustafa Essam <drsh_201122@yahoo.com>
2022-06-30 23:06:04 +0200

    optimize variable storage size

--- fixed_point.hpp
+++ fixed_point.hpp
@@ -3,82 +3,88 @@
 #include <cstddef>
 #include <cstdint>
 
+#if defined(__clang__) || defined(__GNUC__)
+typedef __int128_t int128_t;
+#else
 typedef int int128_t __attribute__((mode(TI)));
+#endif
 
 template<size_t size, size_t dp>
 class FixedPoint {
 
   #pragma pack(push, 1)
-  union _Types {
-    int128_t d1: size;
-    int128_t d2: size*2;
+  struct _Storage {
+    int128_t d: size;
+  };
+  struct _ExtendedStorage {
+    int128_t d: size*2;
   };
   #pragma pack(pop)
 
-  _Types value{0};
-  constexpr _Types _extended() const {
-    _Types t;
-    t.d2 = value.d1;
+  _Storage value{0};
+  constexpr _ExtendedStorage _extended() const {
+    _ExtendedStorage t;
+    t.d = value.d;
     return t;
   }
 public:
   constexpr FixedPoint() = default;
   constexpr FixedPoint(const double d) {
-    value.d1 = int128_t(d * double(1LL << dp) + (d >= 0 ? 0.5 : -0.5));
+    value.d = int128_t(d * double(1LL << dp) + (d >= 0 ? 0.5 : -0.5));
   }
   constexpr operator double() const {
-    return double(value.d1) / double(1LL << dp);
+    return double(value.d) / double(1LL << dp);
   }
 private:
-  static constexpr FixedPoint from(_Types v) { 
+  static constexpr FixedPoint from(_Storage v) { 
     FixedPoint k; 
     k.value = v; 
     return k; 
   }
   static constexpr FixedPoint from(int128_t v) { 
     FixedPoint k; 
-    k.value.d1 = v; 
+    k.value.d = v; 
     return k; 
   }
 public:
   constexpr FixedPoint& operator = (const FixedPoint& f) = default;
   constexpr FixedPoint operator - () const {
-    return from(-this->value.d1);
+    return from(-this->value.d);
   }
   constexpr FixedPoint operator + (const FixedPoint& f) const {
-    return from(this->value.d1 + f.value.d1);
+    return from(this->value.d + f.value.d);
   }
   constexpr FixedPoint& operator += (const FixedPoint& f) {
-    this->value.d1 += f.value.d1;
+    this->value.d += f.value.d;
     return *this;
   }
   constexpr FixedPoint operator - (const FixedPoint& f) const {
-    return from(this->value.d1 - f.value.d1);
+    return from(this->value.d - f.value.d);
   }
   constexpr FixedPoint& operator -= (const FixedPoint& f) {
-    this->value.d1 -= f.value.d1;
+    this->value.d -= f.value.d;
     return *this;
   }
   constexpr FixedPoint operator * (const FixedPoint& f) const {
-    _Types t = _extended();
-    _Types t2 = f._extended();
-    return from((t.d2 * t2.d2) >> dp);
+    auto t = _extended();
+    auto t2 = f._extended();
+    return from((t.d * t2.d) >> dp);
   }
   constexpr FixedPoint& operator *= (const FixedPoint& f) {
-    _Types t = _extended();
-    _Types t2 = f._extended();
-    this->value.d1 = (t.d2 * t2.d2) >> dp;
+    auto t = _extended();
+    auto t2 = f._extended();
+    this->value.d = (t.d * t2.d) >> dp;
     return *this;
   }
   constexpr FixedPoint operator / (const FixedPoint& f) const {
-    _Types t = _extended();
-    _Types t2 = f._extended();
-    return from((t.d2 << dp) / t2.d2);
+    auto t = _extended();
+    auto t2 = f._extended();
+    return from((t.d << dp) / t2.d);
   }
   constexpr FixedPoint& operator /= (const FixedPoint& f) {
-    _Types t = _extended();
-    _Types t2 = f._extended();
-    this->value.d1 = (t.d2 << dp) / t2.d2;
+    auto t = _extended();
+    auto t2 = f._extended();
+    this->value.d = (t.d << dp) / t2.d;
     return *this;
   }
 };
